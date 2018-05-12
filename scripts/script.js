ec4cb7197ac8e6771d7718ef54e86636ce49ffff
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-12 13:29:00 -0400

    finished lesson 6

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
 /*global $*/
+$("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"});
 
-$("h3").css({border: "3px solid blue"});
+$("#contact ul:first").css({border: "2px solid red"});
+$("contact ul:last").css({border: "2px solid red"});
 
-$(".wrapper").css({border: "3px solid red"});
+$("header nav li:even").css({border: "2px solid blue"});
+$("header nav li:odd").css({border: "2px solid yellow"});
 
-$("#clients").css({border: "3px solid yellow"});
+$("section:not('#contact')").css({border: "2px solid green"});
+
+$("#social-nav li:lt(3)").css({border: "2px solid blue"});
+$("#social-nav li:gt(2)").css({border: "2px solid blue"});
+
+$("div[class]").css({border: "2px solid pink"});
+$("img[alt=quote]").css({border: "2px solid purple"});
