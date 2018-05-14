c4b795b7cfb5fb47b1c33eea1a8a732eeb4ae984
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-14 10:54:56 -0400

    finished tutorial 13

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,15 @@
 /*global $*/
 
-// CHANGING ATTRIBUTES
+// CONTROLLING / CHANGING CSS
 
-// $("#contact img").removeAttr("alt");
-console.log($("#contact img").attr("alt"));
+// console.log($("#social-nav").css("top")); // checking value
 
 
-// .removeAttr() - removes an attribute completely
-// .attr() - can read or set any attribute
+
+$("#social-nav").css({
+    "top" : "-400px",
+    "left" : "150px",
+    "opacity" : "0.5",
+    "border-top" : "4px solid red"
+    
+}); // changing value
