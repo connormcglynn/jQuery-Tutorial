5bcd07eb824e5e8d08edcb710da6e3b309a16eb3
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-14 10:38:57 -0400

    finished tutorial 11

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,10 @@
 /*global $*/
 
-// $("section").wrap("<div>");
-// $("section").unwrap();
-// $("section").wrapAll("<div>"); // ONE empty div around all together, not individually
+// REMOVING CONTENT
 
-// WRAPPING AND UNWRAPPING ELEMENTS
+$(".button").empty();
+$(".button").remove();
 
-// wrap() - wraps all matches elements individually
-// unwrap() - unwraps all matches elements
-// wrapALl() - wraps all elements combined with 1 single element
 
-var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
-
-button[0].onclick = function(){
-    if (wrapped) {
-        $("section").unwrap();
-        wrapped = false;
-        button.text("Wrap");
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("Unwrap");
-    }
-};
+// .empty() - empties the innerHTML of an element
+// .remove() - removes the element completely
