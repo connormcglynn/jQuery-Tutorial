f8fb24953f27e2abef437e75e942bfdd8aff7cf6
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-13 23:35:41 -0400

    finished tutorial 7

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    alert('jQuery loaded');
-});
+/*global $*/
+$("#contact-methods").next().css({border: "3px solid red"});
+
+$("#social-nav").prev().css({border: "3px solid blue"});
+
+$(".banner-title").parents().css({border: "3px solid pink"});
+
+$("#social-nav").children().css({border: "3px solid green"});
+
+$("#contact").find(".facebook").css({border: "3px solid purple"});
+
+$("#social-nav").closest(".wrapper").css({border: "3px solid orange"});
